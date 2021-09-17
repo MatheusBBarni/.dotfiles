3c5fbd405e1eaa75da73eaa8f550d3cbd338bf8e
Matheus B. Barni <brehm.matheus@hotmail.com>
2021-09-17 08:30:39 -0300

    Create wsl_vpn.ps1

---
+++ wsl_vpn.ps1
@@ -0,0 +1,2 @@
+Get-NetIPInterface -InterfaceAlias "vEthernet (WSL)" | Set-NetIPInterface -InterfaceMetric 1
+Get-NetAdapter | Where-Object {$_.InterfaceDescription -Match "Cisco AnyConnect"} | Set-NetIPInterface -InterfaceMetric 6000https://github.com/MatheusBBarni/personal-scripts
